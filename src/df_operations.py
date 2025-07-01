a665249a8eee199ae1ca9f955c70f5e71ff521d8
tyamaguchi <takuo6000@gmail.com>
2025-07-01 09:21:51 +0900

    add: implement generalize and replace_nan functions for DataFrame processing

---
+++ src/df_operations.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+NAN_REPLACE_DICT = {
+    'workclass': 'DROP',
+}
+
+def generalize(df: pd.DataFrame, hierarchy_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Generalize the DataFrame based on the provided hierarchy.
+    
+    df: Input DataFrame to be generalized.
+    hierarchy_df: DataFrame containing the hierarchy mapping. Each row should be generalization target, and have col, from, to information.
+    return: Generalized DataFrame.
+
+    hierarchy_df should be like:
+       column        child child_level         parent parent_level
+    workclass  Federal-gov           0  In-government            1
+    workclass    Local-gov           0  In-government            1
+    workclass    State-gov           0  In-government            1
+    ...
+    """
+
+    generalized_df = df.copy()
+
+    # 各カラムについて、hierarchy_dfによる一般化
+    for generalize_col in hierarchy_df['column'].unique():
+        # 一般化規約の抽出
+        mapping = hierarchy_df[hierarchy_df['column'] == generalize_col][['child', 'parent']]
+        # 各列について一般化規則を反映
+        merged = generalized_df[[generalize_col]].merge(mapping, how='left', left_on=generalize_col, right_on='child')
+        generalized_df[generalize_col] = merged['parent']
+
+    print(generalized_df)
+    return generalized_df
+
+def replace_nan(df: pd.DataFrame, replace_dict: dict = NAN_REPLACE_DICT) -> pd.DataFrame:
+    """
+    Replace NaN values in the DataFrame based on the provided dictionary.
+    
+    df: Input DataFrame with potential NaN values (raw adult dataset).
+    replace_dict: Dict to replace NaN, {col_name: replace_value}. If replace_value is 'DROP', the column will be dropped.
+    return: DataFrame with NaN values replaced.
+    """
+    
+    replaced_df = df.copy()
+    drop_cols = [ col for col, value in replace_dict.items() if value == 'DROP' ]
+    print(drop_cols)
+    replaced_df.dropna(subset=drop_cols, inplace=True)
+    replaced_df.fillna(replace_dict, inplace=True)
+    
+    return df
